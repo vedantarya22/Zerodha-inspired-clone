1473ad6539b5eb1b2a814f68dfeb7779990384ea
Vedant Arya <va.vedant@gmail.com>
2026-01-20 03:00:54 +0530

    fixed images bug

--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -9,17 +9,17 @@ function Hero() {
 
             <div className="row p-5 mt-5">
                 <div className="col">
-                    <img src="public\media\pricingEquity.svg" alt="" className='p-5' style={{width:"90%"}}/>
+                    <img src="\media\pricingEquity.svg" alt="" className='p-5' style={{width:"90%"}}/>
                     <h2>Free equity delivery</h2>
                     <p className='text-muted mt-3'>All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
                 </div>
                 <div className="col">
-                    <img src="\public\media\intradayTrades.svg" alt=""  className='p-5' style={{width:"90%"}}/>
+                    <img src="\media\intradayTrades.svg" alt=""  className='p-5' style={{width:"90%"}}/>
                     <h2>Intraday and F&O trades</h2>
                     <p className='text-muted mt-3'>Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
                 </div>
                 <div className="col">
-                    <img src="public\media\pricingEquity.svg" alt=""  className='p-5'style={{width:"90%"}}/>
+                    <img src="\media\pricingEquity.svg" alt=""  className='p-5'style={{width:"90%"}}/>
                     <h2>Free direct MF</h2>
                     <p className='text-muted mt-3'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
                 </div>
--- frontend/src/landing_page/products/ProductsPage.jsx
+++ frontend/src/landing_page/products/ProductsPage.jsx
@@ -10,7 +10,7 @@ function ProductsPage() {
     <>
       <Hero />
       <LeftSection
-        imageUrl="\public\media\kite.png"
+        imageUrl="\media\kite.png"
         productName="Kite"
         productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
         tryDemo=""
@@ -19,7 +19,7 @@ function ProductsPage() {
         appStore=""
       />
       <RightSection
-        imageUrl="\public\media\console.png"
+        imageUrl="\media\console.png"
         productName="Console"
         productDescription="The central dashboard for your Zerodha  account. Gain insights into your trades and investments with in-depth reports and visualisations."
         linkName="Learn more"
@@ -28,7 +28,7 @@ function ProductsPage() {
       
 
       <LeftSection
-        imageUrl="\public\media\coin.png"
+        imageUrl="\media\coin.png"
         productName="Coin"
         productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
         tryDemo=""
@@ -37,7 +37,7 @@ function ProductsPage() {
         appStore=""
       />
   <RightSection
-        imageUrl="\public\media\kiteconnect.png"
+        imageUrl="\media\kiteconnect.png"
         productName="Kite Connect API"
         productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
         linkName="Kite Connect "
@@ -45,7 +45,7 @@ function ProductsPage() {
       />
 
       <LeftSection
-        imageUrl="\public\media\varsity.png"
+        imageUrl="\media\varsity.png"
         productName="Varsity mobile"
         productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
         tryDemo=""
