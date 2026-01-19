bf7f755ca8acdb1d78370c5b38ee98443c3f179f
Vedant Arya <va.vedant@gmail.com>
2026-01-20 01:55:25 +0530

    altered titles

--- dashboard/src/components/Apps.jsx
+++ dashboard/src/components/Apps.jsx
@@ -3,6 +3,7 @@ function Apps() {
     return ( 
         <div>
             <h1>Apps</h1>
+            <p>This page isn’t designed yet but if you hire me  I promise to  finish it before your HR finishes my interview</p>
         </div>
      );
 }
